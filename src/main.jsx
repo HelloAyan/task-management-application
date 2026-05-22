411aa935c4cdc771d65b7bfb97a6ba9f675c1aed
HelloAyan <helloayan171@gmail.com>
2026-05-22 20:35:11 +0600

    feat: vercel speedInsights added

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux'
 import { store } from './redux/store.js'
 import { ToastContainer } from "react-toastify"
 import "react-toastify/dist/ReactToastify.css"
-
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
